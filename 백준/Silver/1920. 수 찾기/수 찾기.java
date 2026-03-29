367d64857623b03b783098f295afa14018807a86
김현주 <126346217+hhyunjju@users.noreply.github.com>
2026-03-30 08:13:10 +0900

    [Silver IV] Title: 수 찾기, Time: 1112 ms, Memory: 62596 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,22 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String[] a = br.readLine().split(" ");
-        Set<String> aSet = Arrays.stream(a).collect(Collectors.toSet());
         int m = Integer.parseInt(br.readLine());
-        String[] inputs = br.readLine().split(" ");
+        String[] mArr = br.readLine().split(" ");
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(Integer.parseInt(a[i]), 0);
+        }
         for (int i = 0; i < m; i++) {
-            int result = aSet.contains(inputs[i]) ? 1 : 0;
-            System.out.println(result);
+            System.out.println(map.containsKey(Integer.parseInt(mArr[i])) ? 1 : 0);
         }
     }
 }
