27e2f14e5480468a08f8bb849751f45b8a40bfb8
김현주 <126346217+hhyunjju@users.noreply.github.com>
2026-06-05 07:45:49 +0900

    [Passed] Factorial (133ms, 10MB)

---
+++ trail2/Factorial/factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.println(factorial(n));
+    }
+    public static int factorial(int x){
+        if(x == 1){
+            return 1;
+        }
+        return x*factorial(x-1);
+    }
+}
