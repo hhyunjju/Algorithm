42fb3b9d4bf616a6904a41a363a70f611f17cc67
김현주 <126346217+hhyunjju@users.noreply.github.com>
2026-07-08 07:47:19 +0900

    [Passed] 그 요일은 (130ms, 10MB)

--- trail2/그 요일은/the-day-of-the-day.java
+++ trail2/그 요일은/the-day-of-the-day.java
@@ -1,29 +1,29 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
         int m1 = sc.nextInt();
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
-        String a = sc.next();
-
-        String[] days = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-
-        // 나눈 몫 = 기본 등장 횟수.
-        int diff = calculateDays(m2,d2) - calculateDays(m1,d1);
-        int aIdx = Arrays.asList(days).indexOf(a);
-        int remain = (aIdx <= (diff%7)) ? 1 : 0;
-        int result = diff/7 + remain;
+        String A = sc.next();
+        // Please write your code here.
+        int dates1 = calculateDates(m1, d1);
+        int dates2 = calculateDates(m2, d2);
+        String[] DAYS = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}; 
+        int result = (dates2-dates1)/7;
+        if((dates2-dates1)%7 >= Arrays.asList(DAYS).indexOf(A)){
+            result++;
+        }
         System.out.println(result);
     }
-    public static int calculateDays(int m, int d){
-        int[] month_days = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int days = 0;
-        for(int i = 1; i < m ; i++){
-            days+=month_days[i];
+    public static int calculateDates(int m, int d){
+        int[] MONTH_DAYS = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int result = 0;
+        for(int i = 1; i < m; i++){
+            result += MONTH_DAYS[i];
         }
-        return days + d;
+        return result + d;
     }
 }
