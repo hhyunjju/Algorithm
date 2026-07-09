eaa5cb3278ef1c7ed4be93b348ccdc0d16bdd5e9
김현주 <126346217+hhyunjju@users.noreply.github.com>
2026-07-10 07:35:19 +0900

    [Passed] 블럭쌓는 명령2 (139ms, 12MB)

---
+++ trail2/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n+1];
+        for(int i = 0 ; i < k ; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            for(int j = a ; j <= b ; j++){
+                arr[j] += 1;
+            }
+        }
+
+        int max = 0;
+        for(int l = 0 ; l < arr.length ; l++){
+            max = Math.max(max, arr[l]);
+        }
+        System.out.println(max);
+    }
+}
