f06859b25eec6ac7708a0f14fd438bda772c98bf
김현주 <126346217+hhyunjju@users.noreply.github.com>
2026-07-14 07:42:58 +0900

    [Passed] T를 초과하는 연속 부분 수열 (130ms, 10MB)

---
+++ trail2/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int max = 0;
+        int cnt = 0;
+        for(int i = 0 ; i<arr.length ; i++){
+            if(arr[i] > t){
+                cnt++;
+                max = Math.max(max, cnt);
+            }else{
+                cnt = 0;
+            }
+        }
+        System.out.println(max);
+    }
+}
