4207cd734190a32001e1188dc08d8eac96123db0
김현주 <126346217+hhyunjju@users.noreply.github.com>
2026-07-20 07:53:23 +0900

    [Passed] 거울에 레이저 쏘기 2 (169ms, 11MB)

--- trail2/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
+++ trail2/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -1,124 +1,66 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    private static char[][] arr;
+    private static char[][] grid;
+    private static int[] dr = {-1, 1, 0, 0}; // 위 아래 왼 오
+    private static int[] dc = {0, 0, -1, 1};
+    private static int[] slash = {3, 2, 1, 0}; // / -> 아래 -> 왼 / 오 -> 위 / 왼 -> 아래 / 위 -> 오
+    private static int[] backslash = {2, 3 , 0, 1}; // \\ -> 아래 -> 오/ 왼 -> 위 / 위 -> 왼 / 아래 -> 오
     
     private static int dir;
     private static int r;
     private static int c;
 
-    private static int[] dr = {-1, 1, 0, 0}; // 위 아래 왼쪽 오른쪽 0<>3 / 1<>2
-    private static int[] dc = {0, 0, -1, 1}; // 1<>3 0<>2
-
-    private static int[] slash = {3, 2, 1, 0};      // '/' 반사
-    private static int[] backslash = {2, 3, 0, 1};  // '\' 반사
-
     private static boolean isRange(int r, int c){
-        return r>=0 && r< arr.length && c>= 0 && c < arr[0].length;
+        return r >= 0 && r < grid.length && c >= 0 && c < grid[0].length;
     }
 
-    private static void initialize(int k, int n){
-        if(k<=n){
+    private static void initialzie(int startNum, int n){
+        if(startNum <= n){
             dir = 1;
             r = 0;
-            c = k-1;
-        }else if(k <= 2*n){
+            c = startNum -1;
+        }else if(startNum <= 2*n){
             dir = 2;
-            r = (k-n)-1;
+            r = (startNum-n) -1;
             c = n-1;
-        }else if(k <= 3*n){
+
+        }else if(startNum <= 3*n){
             dir = 0;
             r = n-1;
-            c = 3*n - k;
+            c = 3*n - startNum;
         }else{
             dir = 3;
-            r = 4*n - k;
+            r = 4*n - startNum;
             c = 0;
         }
     }
 
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        arr = new char[n][n];
-        // 거울에 튕기는 횟수 구하기
-        for(int r= 0 ; r < n ; r++){
-            char[] rowArr = sc.next().toCharArray();
-            for(int c = 0 ; c < n ; c++){
-                arr[r][c] = rowArr[c];
+        grid = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = s.charAt(j);
             }
         }
-        int k = sc.nextInt();
-        
-        // 시작 위치와 방향 추출
-        initialize(k, n);
+        int startNum = sc.nextInt();
+        // Please write your code here.
+        initialzie(startNum, n);
 
-        // (r,c)에서 dir 방향으로 시작하여 시뮬레이션 진행
         int cnt = 0;
         while(isRange(r, c)){
-            // next 로 move
-            if(arr[r][c] == '/'){
+            cnt++;
+            if(grid[r][c] == '/'){
                 dir = slash[dir];
-                r += dr[dir];
-                c += dc[dir];
             }else{
                 dir = backslash[dir];
-                r += dr[dir];
-                c += dc[dir];
             }
-            cnt++;
+            r += dr[dir];
+            c += dc[dir];
         }
-        System.out.println(cnt); 
-
-        //-----------------------------------------
-        // // 한번에 방향전환 + 이동가지
-        // // 범위를 벗어나면 종료
-
-        // // D L U R 
-        // // 123 / 456 / 987 / 101112
-
-        // // 시작방향
-        // int dir = (k-1+4)%4-1;
-        // dr = new int[]{1, 0, -1, 0};
-        // dc = new int[]{0, -1, 0, 1};
-
-        // // 튕기는 횟수 (점유 횟수)
-        // int cnt = 0;
-
-        // // k의 위치 구하기
-        // int r = 0;
-        // int c = 0;
-        // if(dir == 0){
-        //     r = 0;
-        //     c = k-1;
-        // }else if(dir == 2){
-        //     r = n-1;
-        //     c = k-1;
-        // }else if(dir == 1){
-        //     r = (k%n)-1;
-        //     c = n-1;
-        // }else{
-        //     // dir == 3
-        //     r = n - ((k%n)-1);
-        //     c = 0;
-        // }
-
-        // while(true){
-        //     if(arr[r][c] == '/'){ // 반시계
-        //         dir = (dir+3)%4;
-        //     }else{
-        //         dir = (dir+1)%4;
-        //     }
-
-        //     if(!isRange(r + dr[dir], c + dc[dir])){
-        //         break;
-        //     }
-        //     r = r + dr[dir];
-        //     c = c + dc[dir];
-        //     cnt++;
-
-        // }
-        // System.out.println(cnt);
+        System.out.println(cnt);
     }
 }
