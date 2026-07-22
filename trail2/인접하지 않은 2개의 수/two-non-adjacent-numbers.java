9a6e6c1a5571105757d3c1e497fb5abddbacff1f
김현주 <126346217+hhyunjju@users.noreply.github.com>
2026-07-23 07:44:59 +0900

    [Passed] 인접하지 않은 2개의 수 (155ms, 10MB)

---
+++ trail2/인접하지 않은 2개의 수/two-non-adjacent-numbers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int max = 0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i+2; j < n ;j++){
+               max = Math.max(max, arr[i] + arr[j]);
+            }
+        }
+        System.out.println(max);
+    }
+}
