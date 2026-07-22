f39d9128360acbeebfbf76ab1a0ad89de6f15f2b
김현주 <126346217+hhyunjju@users.noreply.github.com>
2026-07-23 07:39:23 +0900

    [Passed] 괄호 쌍 만들어주기 2 (142ms, 10MB)

---
+++ trail2/괄호 쌍 만들어주기 2/pair-parentheses-2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        char[] arr = sc.next().toCharArray();
+
+        int cnt = 0;
+        for(int i = 0 ; i < arr.length-3 ; i++){
+            if(arr[i] == '(' && arr[i+1] == '('){
+                for(int j = i+2; j < arr.length-1 ; j++){
+                    if(arr[j] == ')' && arr[j+1] == ')'){
+                        cnt++;
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
